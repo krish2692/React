1214b618592846a3f843fe6165b6831134852367
Krishna Surti <krishsurti2692@gmail.com>
2022-08-25 19:29:47 -0500

    Added 3 components to start with

--- React Projects/airbnb-exp/src/App.js
+++ React Projects/airbnb-exp/src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Navbar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className="nav">
+      <img src="/assets/air-bnb-logo.png" alt='Airbnb logo'></img>
+    </nav>
+  );
+}
+
+function GridIntro() {
+  return (
+    <div className='grid-intro' id='grid'>
+      <img src='/assets/img-grid.png' alt='grid image'></img>
+      <div className='intro-text'>
+        <h2>Online Experiences</h2>
+        <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Card() {
+  return (
+    <div className='cards'>
+      <img className='person' src='/assets/img-person1.png' alt='person image'></img>
+      <div className='rating-reviews'>
+        <img className='stars' src='/assets/star 1.png' alt='rating'></img>
+        <span className='rating'>5.0</span>
+        <span className='reviews'>(6)</span>
+        <span className='country-info'>-USA</span>
+      </div>
+      <span className='title'>
+        Life Lessons with Katie
+      </span>
+      <div>
+        <span className='price'>
+          From $136 /
+        </span>
+        <span className='title'>
+          person
+        </span>
+      </div>
+    </div>
+  );
+}
+
+function Hero() {
+  return (
+    <div className='flex'>
+      <Card />
+      <Card />
+    </div>
+  );
+}
+  export { Navbar, GridIntro, Card, Hero };
